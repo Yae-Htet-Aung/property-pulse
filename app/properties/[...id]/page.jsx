c81741e9d342bdef4288331d43953a34855c11fd
Yae-Htet-Aung <yaetactaung@gmail.com>
2024-06-23 18:13:22 +0630

    client side vs server side and next-navigation

--- app/properties/[...id]/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PropertySpread = () => {
-  return <div>PropertySpread</div>;
-};
-
-export default PropertySpread;
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
 
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  const router = useRouter();
+  const { id } = useParams();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const name = searchParams.get("who");
+
+  console.log("Hello, id is", id);
+  console.log("Name is", name);
+  console.log("Path name is", pathName);
+  return (
+    <div>
+      <button
+        onClick={() => router.push("/")}
+        className="bg-blue-500 p-3 rounded-md"
+      >
+        Go Home
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
