3ea7588299ca62884bd5f3051049b15a6fb648bb
Yae-Htet-Aung <yaetactaung@gmail.com>
2024-06-23 17:34:54 +0630

    create layout, homepage and style

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import "@/assets/styles/global.css";
 
-const MainLayout = () => {
+export const metadata = {
+  title: "Property Pulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
+const MainLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
-        <div>Main Layout</div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 };
